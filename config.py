cdb26496aa97e6206c314a6f873a31502b7b1567
ujju4155 <161287283+ujju4155@users.noreply.github.com>
2024-04-01 18:24:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/84819fc115cb0eff32b2b.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/76b1bd3adb18fed87ce32.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/76b1bd3adb18fed87ce32.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tbcbotschat")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tbc_bots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/toman07")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/toman07")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5090817443").split()))
 
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://telegra.ph/file/76b1bd3adb18fed87ce32.jpg"
